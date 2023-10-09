a8ce4bbe57a0d31fc1e978ee835e91830f01f994
igorwgsilva <110496185+igorwgsilva@users.noreply.github.com>
2023-10-09 19:16:46 -0300

    Create 03.c

---
+++ Listas de exercício/Lista 1 - Estruturas Condicionais/03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	float x;
+
+	printf("Insira a temperatura em Fahrenheit:\n");
+	scanf("%f", &x);
+
+	printf("\n\tValor em Celsius: %f", x = 5*((x-32)/9));
+
+return 0;
+}
