f48f6c79d07e54abba9746ee262067ee8b7f57fb
igorwgsilva <110496185+igorwgsilva@users.noreply.github.com>
2023-10-09 19:36:28 -0300

    Create 05.c
    Crie um programa em linguagem C que receba um tempo em anos e imprima quantos dias são equivalentes ao tempo informado. Ex. 3 anos equivalem a 1095 dias.

---
+++ Listas de exercício/Lista 1 - Estruturas Condicionais/05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+	float x, y;
+
+	printf("Insira um valor em anos para conversão:\n");
+	scanf("%f", &x);
+
+	printf("Insira quantos anos bissextos há:\n");
+	scanf("%f", &y);
+
+  printf("\n\tValor em dias: %f", x = (x*365+y));
+	
+  return 0;
+}
