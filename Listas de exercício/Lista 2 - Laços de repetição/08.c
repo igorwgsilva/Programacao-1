a8fe02c6ce236263fc6afe266d431ade7d1130c2
igorwgsilva <110496185+igorwgsilva@users.noreply.github.com>
2023-10-13 21:26:28 -0300

    Create 08.c

---
+++ Listas de exercício/Lista 2 - Laços de repetição/08.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void){
+
+  int x=0, y=0;
+
+  while(x!=5){
+    printf("\n\t\tMenu:\n\t1 - Opção 1\n\t2 - Opção 2\n\t3 - Opção 3\n\t4 - Opção 4\n\t5 - Sair\n");
+    scanf("%d", &x);
+
+    switch (x){
+      case 1:
+      printf("\nOpção 1\n");
+      break;
+
+      case 2:
+      printf("\nOpção 2\n");
+      break;
+
+      case 3:
+      printf("\nOpção 3\n");
+      break;
+
+      case 4:
+      printf("\nOpção 4\n");
+      break;
+    }
+  }
+
+  return 0;
+}
